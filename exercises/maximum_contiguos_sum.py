deff22a8eefbc2052059d5fa807d80ae987b5ce1
Katyanna Moura <amelie.kn@gmail.com>
2019-02-27 17:27:05 +0100

    Fix comments formatting

--- exercises/maximum_contiguos_sum.py
+++ exercises/maximum_contiguos_sum.py
@@ -1,7 +1,12 @@
-#2. For the given array which consists of integers, write a function that finds 
-#the sum of a contiguous subarray of numbers which has the largest sum.
-#For example:  Input : [2, 3, -44, 99, 13, -5, 32 , -16, 5, 7]
-#Output: 139    ( 99 + 13 + -5 + 32)
+'''
+For the given array which consists of integers, write a function that finds 
+the sum of a contiguous subarray of numbers which has the largest sum.
+
+For example:
+    Input : [2, 3, -44, 99, 13, -5, 32 , -16, 5, 7]
+    Output: 139    ( 99 + 13 + -5 + 32)
+'''
+
 
 def max_cont_sum(arr, size):
     latest_max = arr[0]
